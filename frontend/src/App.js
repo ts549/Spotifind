e26d8c7eb6542dfca066279a9f3e9b470aac7675
ts549 <sun955@purdue.edu>
2022-08-15 18:55:49 -0400

    fml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,22 +10,8 @@ function App() {
 
   return (
     <div>
-      <Background />
-      <div class="relative w-screen h-screen flex items-center justify-center">
-        <div class="relative ">
-          <div class="pb-2"> 
-            <Vinyl/>
-          </div>
-          <div class="absolute right-44 bottom-0"> 
-            <Square/>
-          </div>
-        </div>
-      </div>
-
-     
+      <Background />     
     </div>
-
-    
   
   );
 }
--- frontend/src/components/backdrop.js
+++ frontend/src/components/backdrop.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const backdrop = () => {
+function Backdrop() {
+
+  const [changed, setChanged] = useState(false);
+
+  function clicked() {
+    if (!changed) {
+      setChanged(true);
+    }
+  }
+
   return (
-    <div class="bg-purpledge w-full h-screen font-sans">
-        <div class="text-white text-left pl-6 pt-5 text-3xl"> 
+    <div className={"bg-light_red relative duration-500 " + (changed ? "w-96 h-96 mx-auto my-[9%]" : "w-screen h-screen")}>
+        <div className={"text-white text-left pl-[3%] pt-[2%] text-3xl absolute z-10 duration-500" + (changed ? "text-3xl" : "text-xl")}> 
             mood
         </div>
-        
-        <div class="text-center">
-            center widget
+
+        <div class="justify-center items-center flex absolute z-0 inset-0">
+            <button className={"text-white duration-500 " + (changed ? "text-xl" : "text-2xl")} onClick={clicked}>Click Me</button>
         </div>
-        
- 
-    </div>
+      </div>
   )
 }
 
-export default backdrop
+export default Backdrop
