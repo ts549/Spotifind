70a26af9c38d2c6b7da8f5034cf39f6a03c5fb64
ts549 <sun955@purdue.edu>
2022-08-16 20:09:31 -0400

    xie chuen

--- frontend/src/components/background.js
+++ frontend/src/components/background.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Backdrop from './backdrop';
+import Vinyl from './vinyl';
 
 function Background() {
     return (
         <div className="fixed h-screen w-screen">
             <p className= "text-black absolute flex items-left text-left pl-[3%] pt-[2%] font-bold text-3xl">mood</p>
-            <div>
-                <Backdrop className="relative"/>
+            
+            <div className="relative w-screen h-screen flex items-center justify-center z-10">
+                <div className="relative top-[11.4%]">
+                    <div className="pb-2">
+                        <Vinyl />
+                    </div>
+                </div>
+
+                <div className="absolute bottom-0">
+                    <Backdrop className="relative z-0"/>
+                </div>  
             </div>
-            {/* <div>
-                <p className="absolute">mood</p>
-                <Backdrop className="absolute"/>
-            </div> */}
+            
         </div>
     )
 }
